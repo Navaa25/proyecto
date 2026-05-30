5da412cd56fc6cdb670d2f9a4501cc3b2fbaadf7
Navaa25 <l22020048@veracruz.tecnm.mx>
2026-05-30 03:37:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,17 @@ import os
 
 app = Flask(__name__, template_folder='.')
 
-# Conexión directa a tu base de datos real de MongoDB Atlas
+# Conexión con mongoDB
 MONGO_URI = "mongodb+srv://ajaviermonna5_user:250203ja@cluster0.ebicbgb.mongodb.net/?appName=Cluster0"
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
 db = client["Chedraui_Simulador"]
 coleccion_ventas = db["ventas"] # Se guardará en una nueva colección llamada ventas
 
-# Ruta para servir tu página web index.html de forma automática
+#automática
 @app.route('/')
 def index():
     return send_from_directory('.', 'index.html')
 
-# API que recibe el JSON del Punto de Venta y lo manda a la nube
 @app.route('/api/ventas', methods=['POST'])
 def guardar_venta():
     try:
@@ -31,6 +30,6 @@ def guardar_venta():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    # Render nos asigna un puerto automático
+    # puerto automático
     puerto = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=puerto)
